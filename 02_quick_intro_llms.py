a8ca2d0d7c2bb770055e64f6ad3937e7a4394f25
Hugh Grigg <hugh.g@elsewhen.com>
2024-07-17 18:33:29 +0100

    Multi question HF example

--- 02_quick_intro_llms.py
+++ 02_quick_intro_llms.py
@@ -26,5 +26,37 @@ def question_answer_prompt_template_example():
     print(llm_chain.run(question))
 
 
+def multiple_questions_example():
+    template = """Question: {question}
+
+        Answer: """
+
+    prompt = PromptTemplate(
+        template=template,
+        input_variables=["question"],
+    )
+
+    hub_llm = HuggingFaceHub(
+        repo_id="google/flan-t5-large", model_kwargs={"temperature": 0}
+    )
+
+    llm_chain = LLMChain(
+        prompt=prompt,
+        llm=hub_llm,
+    )
+
+    qa = [
+        {"question": "What is the capital city of France?"},
+        {"question": "What is the largest mammal on Earth?"},
+        {"question": "Which gas is most abundant in Earth's atmosphere?"},
+        {"question": "What color is a ripe banana?"},
+    ]
+
+    res = llm_chain.generate(qa)
+
+    print(res)
+
+
 if __name__ == "__main__":
-    question_answer_prompt_template_example()
+    # question_answer_prompt_template_example()
+    multiple_questions_example()
