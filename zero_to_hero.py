0c196163f080557ad647d22dbb458a3b15dadd71
Hugh Grigg <hugh.g@elsewhen.com>
2024-07-17 13:35:03 +0100

    Zero to hero conversation example

--- zero_to_hero.py
+++ zero_to_hero.py
@@ -1,6 +1,8 @@
-from langchain.prompts import PromptTemplate
-from langchain.llms import OpenAI
+from langchain.chains import ConversationChain
 from langchain.chains import LLMChain
+from langchain.llms import OpenAI
+from langchain.memory import ConversationBufferMemory
+from langchain.prompts import PromptTemplate
 
 
 def direct_llm_example():
@@ -26,6 +28,22 @@ def prompt_template_example():
     print(chain.run("eco-friendly water bottles"))
 
 
+def conversation_example():
+    llm = OpenAI(model="gpt-3.5-turbo-instruct", temperature=0)
+
+    conversation = ConversationChain(
+        llm=llm, verbose=True, memory=ConversationBufferMemory()
+    )
+
+    conversation.predict(input="Tell me about yourself.")
+
+    conversation.predict(input="What can you do?")
+    conversation.predict(input="How can you help me with data analysis?")
+
+    print(conversation)
+
+
 if __name__ == "__main__":
     # direct_llm_example()
-    prompt_template_example()
+    # prompt_template_example()
+    conversation_example()
