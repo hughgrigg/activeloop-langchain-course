450d4a5c3b6f69bca5340c8dcb3915a618525858
Hugh Grigg <hugh.g@elsewhen.com>
2024-07-17 17:19:16 +0100

    Zero to hero agent tools Google search example

--- zero_to_hero.py
+++ zero_to_hero.py
@@ -3,12 +3,15 @@ Requires the following env vars:
  - OPENAI_API_KEY
  - ACTIVELOOP_TOKEN
  - ACTIVELOOP_ORG_ID
+ - GOOGLE_API_KEY
+ - GOOGLE_CSE_ID
 """
 
 import os
 
 from langchain.agents import AgentType
-from langchain.agents import initialize_agent, Tool
+from langchain.agents import Tool
+from langchain.agents import initialize_agent
 from langchain.chains import ConversationChain
 from langchain.chains import LLMChain
 from langchain.chains import RetrievalQA
@@ -17,6 +20,7 @@ from langchain.llms import OpenAI
 from langchain.memory import ConversationBufferMemory
 from langchain.prompts import PromptTemplate
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.utilities import GoogleSearchAPIWrapper
 from langchain.vectorstores import DeepLake
 
 
@@ -147,9 +151,36 @@ def deeplake_existing_dataset_example():
     print(response)
 
 
+def agent_tools_google_search_example():
+    llm = OpenAI(model="gpt-3.5-turbo-instruct", temperature=0)
+
+    search = GoogleSearchAPIWrapper()
+
+    tools = [
+        Tool(
+            name="google-search",
+            func=search.run,
+            description="useful for when you need to search google to answer questions about current events",
+        )
+    ]
+
+    agent = initialize_agent(
+        tools,
+        llm,
+        agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+        verbose=True,
+        max_iterations=6,
+    )
+
+    response = agent("What's the latest news about the Mars rover?")
+
+    print(response["output"])
+
+
 if __name__ == "__main__":
     # direct_llm_example()
     # prompt_template_example()
     # conversation_example()
     # deeplake_new_dataset_example()
-    deeplake_existing_dataset_example()
+    # deeplake_existing_dataset_example()
+    agent_tools_google_search_example()
